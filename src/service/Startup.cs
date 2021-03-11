396a587e77dcec38dbbe032e7899e01858b7c293
b9b <a951852753@hotmail.com>
2021-03-12 00:56:09 +0800

    feat: create database
    createt database after start service

--- src/service/Startup.cs
+++ src/service/Startup.cs
@@ -21,6 +21,12 @@ namespace service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                using var context = scope.ServiceProvider.GetRequiredService<DictionaryContext>();
+                context.Database.Migrate();
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
